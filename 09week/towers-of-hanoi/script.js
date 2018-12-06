197ca17a4ab5bc257793f850b18fc228794cf2be
Drew McPherson <drewmac1971@gmail.com>
2018-12-05 18:23:51 -0600

    first commit for towers of hanoi

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -2,4 +2,14 @@
 
 $(document).ready(function() {
   // Put app logic here
+  $(function(){
+    $(".draggable").draggable({
+      // revert: 'invalid'
+    });
+    $('#dropArea').droppable({
+      drop: function( event, ui ) {
+        $(ui.draggable).appendTo(this).attr('style', 'position: "relative"');
+      }
+    })
+  })
 });
