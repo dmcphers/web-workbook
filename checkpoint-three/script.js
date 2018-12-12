4700e8ff2c04a067015aceba4086e63973f33a9a
Drew McPherson <drewmac1971@gmail.com>
2018-12-11 23:54:34 -0600

    making changes to checkpoint 3

--- checkpoint-three/script.js
+++ checkpoint-three/script.js
@@ -1,91 +1,170 @@
 $(document).ready(function(){
-    var tot1 = 0;
-    var tot2 = 0;
+    // hide the dice
+    document.querySelector('.dice1').style.display = 'none';
+    document.querySelector('.dice2').style.display = 'none';
+    // reset all scores and current
+    $('#current-score-1').text('0');
+    $('#current-score-2').text('0');
+    $('#total-score-1').text('0');
+    $('#total-score-2').text('0');
+    // set active class to active player 1 area
+    $('.player-1-area').addClass('active');
+    // disable player 2 roll dice button
+    $("#btn-roll-2").prop("disabled",true);
+    currscore1 = 0;
+    currscore2 = 0;
+    totscore1 = 0;
+    totscore2 = 0;
+    var printDiceCurr;
+    var printDiceTotal;
     var turn = 'p1';
     var sides = 6;
-    var dice = {
+    var dice1 = {
        sides: sides,
        roll: function() {
          var randomNumber = Math.floor(Math.random() * this.sides) + 1;
          return randomNumber;
          }
     }
+    var dice2 = {
+        sides: sides,
+        roll: function() {
+          var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+          return randomNumber;
+          }
+     }    
+     
     
-   $("#button").on("click",function(){
-       if(turn === 'p2'){
-      $("#button").prop("disabled",true);
-             }
+   $("#btn-roll-1").on("click",function(){
+        if(turn === 'p1'){
+        $("#btn-roll-1").prop("disabled",true);
+        }
+
+    // get winning score
+    var input = $('.winningScore').val();
+    var winningScore;
+    // if there is an input
+    if(input) {
+       winningScore = input;
+    } else {
+      // without input default score
+      winningScore = 50;
+    }
+
+    var result1 = dice1.roll(); 
+    var result2 = dice2.roll(); 
+    printNumber(result1,result2);
      
-    var result = dice.roll(); 
-    printNumber(result);
-     
-    tot1 = result + tot1;
-    var printDiceTotal = "Total: " + tot1;
-    $("#diceTotal").text(printDiceTotal);
+    currscore1 = result1 + result2;
+    printDiceCurr = currscore1;
+    totscore1 = result1 + result2 + totscore1;
+    printDiceTotal = totscore1;
+    $("#current-score-1").text(printDiceCurr);
+    $("#total-score-1").text(printDiceTotal);
     
-    turn = 'p2';
-    $("#p2turn").text("Player 2 turn");
-    $("#p1turn").text("");
-    $("#button2").prop("disabled",false);
-    $("#button").prop("disabled", true);
+    if (totscore1 > winningScore){
+        // Check if player won the game based on target score
+        $('#name-1').text('Winner!');
+        //disable roll dice buttons
+        $("#btn-roll-1").prop("disabled",true);
+        $("#btn-roll-2").prop("disabled",true);
+    } else {
+        turn = 'p2';
+        $("#btn-roll-1").prop("disabled",true);
+        $("#btn-roll-2").prop("disabled", false);
+    }
+
+        // toggle active areas
+        $('.player-1-area').toggleClass('active');
+        $('.player-2-area').toggleClass('active');
      
-    if (tot1 > 20){
-      $("#win").text("Player 1 Wins!");
-      $("#p1turn").text("");
-      $("#p2turn").text("");
-      $("#button").prop("disabled",true);
-      $("#button2").prop("disabled",true);
-      }
    });
     
-    function printNumber(number) {
-       $("#dice").text(number);
+    function printNumber(number1,number2) {
+        var diceDOM1 = document.querySelector('.dice1');
+        var diceDOM2 = document.querySelector('.dice2');
+        diceDOM1.style.display = 'block';
+        diceDOM2.style.display = 'block';
+        diceDOM1.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/243004/dice-' + number1 + '.png';
+        diceDOM2.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/243004/dice-' + number2 + '.png';
      }
     
-    $("#button2").on("click",function(){
-       if(turn === 'p1'){
-         $("#button2").prop("disabled",true);
-             }
-    var result = dice.roll();
-    printNumber2(result);
-      
-    tot2 = result + tot2;
-    var printDiceTotal = "Total: " + tot2;
-    $("#diceTotal2").text(printDiceTotal);
-   
-    turn = 'p1';
-    $("#p2turn").text("");
-    $("#p1turn").text("Player 1 turn");
-    $("#button2").prop("disabled",true);
-    $("#button").prop("disabled",false);
-      
-     if (tot2 > 20){
-      $("#win2").text("Player 2 Wins!");
-      $("#p1turn").text("");
-      $("#p2turn").text("");
-      $("#button").prop("disabled",true);
-      $("#button2").prop("disabled",true);
-       }
+    $("#btn-roll-2").on("click",function(){
+        if(turn === 'p2'){
+            $("#btn-roll-2").prop("disabled",true);
+                   }
+
+        // get winning score
+        var input = $('.winningScore').val();
+        console.log(input);
+        var winningScore;
+        // if there is an input
+        if(input) {
+            winningScore = input;
+        } else {
+            // without input default score
+            winningScore = 50;
+        }
+           
+        var result1 = dice1.roll(); 
+        var result2 = dice2.roll(); 
+        printNumber(result1,result2);
+        
+        currscore2 = result1 + result2;
+        printDiceCurr = currscore2;
+        totscore2 = result1 + result2 + totscore2;
+        printDiceTotal = totscore2;
+        $("#current-score-2").text(printDiceCurr);
+        $("#total-score-2").text(printDiceTotal);
+          
+        if (totscore2 > winningScore){
+            // Check if player won the game based on target score
+            $('#name-2').text('Winner!');
+            //disable roll dice buttons
+            $("#btn-roll-1").prop("disabled",true);
+            $("#btn-roll-2").prop("disabled",true);
+        } else {
+            turn = 'p1';
+            $("#btn-roll-1").prop("disabled",false);
+            $("#btn-roll-2").prop("disabled", true);
+        }
+            // toggle active areas
+            $('.player-1-area').toggleClass('active');
+            $('.player-2-area').toggleClass('active');
+          
     });
     
-    function printNumber2(number) {
-     $("#dice2").text(number);
-     }
     
-    
-  $("#newgame").on("click",function(){
-      $("#dice").text("");
-      $("#dice2").text("");
-      $("#diceTotal").text("");
-      $("#diceTotal2").text("");
-      $("#p1turn").text("");
-      $("#p2turn").text("");
-      $("#win").text("");
-      $("#win2").text("");
-      $("#button").prop("disabled",false);
-      $("#button2").prop("disabled",false);
-      tot1 = 0;
-      tot2 = 0;
-      var turn = 'p1';
-     })
+    $(".btn-new").on("click",function(){
+        // clear the score input field
+        $('.winningScore').val('');
+        // hide the dice
+        document.querySelector('.dice1').style.display = 'none';
+        document.querySelector('.dice2').style.display = 'none';
+        // reset all scores and current
+        $('#current-score-1').text('0');
+        $('#current-score-2').text('0');
+        $('#total-score-1').text('0');
+        $('#total-score-2').text('0');
+        // change player names back
+        $('#name-1').text('Player 1');
+        $('#name-2').text('Player 2');
+        // remove winner class to active player panel
+        $('.player-1-area').removeClass('winner');
+        $('.player-2-area').removeClass('winner');
+        // reset active class to active player panel
+        $('.player-1-area').removeClass('active');
+        $('.player-2-area').removeClass('active');
+        $('.player-1-area').addClass('active');
+        $("#btn-roll-1").prop("disabled",false);
+            // disable player 2 roll dice button
+        $("#btn-roll-2").prop("disabled",true);
+        currscore1 = 0;
+        currscore2 = 0;
+        totscore1 = 0;
+        totscore2 = 0;
+        var turn = 'p1';
+    })
+
  });
+
